cb2fd764a897294093f808139793021b8a55444e
harryzsh <shz@microsoft.com>
2020-04-08 20:53:26 +0800

    This is a test commit

--- scripts/capture.bat
+++ scripts/capture.bat
@@ -1,7 +1,7 @@
 @REM Capture the content of an API Management portal into data_file - incl. pages, layouts, configuration, etc. but excluding media files
 
-set management_endpoint="< service name >.management.azure-api.net"
-set access_token="SharedAccessSignature ..."
-set data_file="../dist/data.json"
+set management_endpoint="management.harrytest.xyz"
+set access_token="SharedAccessSignature integration&202005071123&M7jkNTYMWWo9NuDEJDhJtALKlBZNYYocKQpnO8gkj9YwC/v+vwsx2FzbKrthdP/AWBuzXBcSw8yw662UsLtFQA=="
+set data_file="../dist/harryDevPortalBackup_data.json"
 
 node ./capture %management_endpoint% %access_token% %data_file%
--- scripts/generate.bat
+++ scripts/generate.bat
@@ -1,9 +1,10 @@
 @REM Generate and provision default content of an API Management portal - incl. pages, layouts, configuration, media files, etc.
 
-set management_endpoint="< service name >.management.azure-api.net"
-set access_token="SharedAccessSignature ..."
-set storage_connection_string="DefaultEndpointsProtocol=..."
-set data_file="./data.json"
+set management_endpoint="management.harrytest.xyz"
+set access_token="SharedAccessSignature integration&202005071123&M7jkNTYMWWo9NuDEJDhJtALKlBZNYYocKQpnO8gkj9YwC/v+vwsx2FzbKrthdP/AWBuzXBcSw8yw662UsLtFQA=="
+set storage_connection_string="DefaultEndpointsProtocol=https;AccountName=harryapimstorage;AccountKey=IW4PI0LKtCDDjWsPFk7d87dRaq8UTRU2Ok6Ecp0Q8PvuBuLx0e9hvV0F5wow0QJCfbSLUWKGqIRII63bF6s25g==;EndpointSuffix=core.windows.net"
+
+set data_file="../dist/harryDevPortalBackup_data.json"
 set media_folder="./media"
 
 node ./generate %management_endpoint% %access_token% %data_file%
